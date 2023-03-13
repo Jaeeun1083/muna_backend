8b3a6a702b3518754f8483dcc258adcdd6324894
Jaeeun <jjomang7602@naver.com>
2023-03-13 15:41:22 +0900

    feat: DB 연동 (mybatis) 및 yaml 파일 암호화 (jasypt)

--- src/main/java/com/life/muna/MunaBackendApplication.java
+++ src/main/java/com/life/muna/MunaBackendApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MunaBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MunaBackendApplication.class, args);
+		try {
+			SpringApplication.run(MunaBackendApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
