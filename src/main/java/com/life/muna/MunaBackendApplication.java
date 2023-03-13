2cdb7dc471c4335c74724b1f3a6c92c16b2fdae0
Jaeeun <jjomang7602@naver.com>
2023-03-13 13:55:16 +0900

    Initial commit

---
+++ src/main/java/com/life/muna/MunaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.life.muna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/life/muna/MunaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.life.muna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
