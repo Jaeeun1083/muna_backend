bf5bf6e4369c50204b5080d481bf0da5f758d953
Jaeeun <jjomang7602@naver.com>
2023-10-08 00:22:27 +0900

    feat: localDateTime => String 변환 기능 추가

--- src/main/java/com/life/muna/common/util/TimeConverter.java
+++ src/main/java/com/life/muna/common/util/TimeConverter.java
@@ -1,14 +1,20 @@
 package com.life.muna.common.util;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class TimeConverter {
-    private static final String DATE_TIME_FORMAT = "yyyy.MM.dd HH:mm:ss";
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-    public static String convert(Date date) {
+    public static String convertDate(Date date) {
         SimpleDateFormat formatter = new SimpleDateFormat(DATE_TIME_FORMAT);
         return formatter.format(date);
     }
 
+    public static String convertDate(LocalDateTime date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
+        return date.format(formatter);
+    }
 }
