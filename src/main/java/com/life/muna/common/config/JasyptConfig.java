c00b49982d4d9880125f37be62c0d6dc84a67e17
Jaeeun <jjomang7602@naver.com>
2023-10-08 00:16:09 +0900

    fix: 로컬 db 접속 정보로 변경

--- src/main/java/com/life/muna/common/config/JasyptConfig.java
+++ src/main/java/com/life/muna/common/config/JasyptConfig.java
@@ -4,6 +4,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.jasypt.salt.StringFixedSaltGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,7 +20,7 @@ public class JasyptConfig {
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setSaltGenerator(new StringFixedSaltGenerator("someFixedSalt"));
         config.setStringOutputType("base64");
         encryptor.setConfig(config);
         return encryptor;
